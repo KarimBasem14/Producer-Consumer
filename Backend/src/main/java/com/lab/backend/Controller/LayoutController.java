1a5a62075287ed3208a84063c0a9bc05069d27c8
Esraa Abdelhay Mohamed Mohamed <esraaabdelhay1984@gmail.com>
2025-12-26 09:38:24 +0200

    modification on layout controller

--- Backend/src/main/java/com/lab/backend/Controller/LayoutController.java
+++ Backend/src/main/java/com/lab/backend/Controller/LayoutController.java
@@ -9,6 +9,8 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@CrossOrigin(origins = "http://localhost:4200")
+@RequestMapping("/layout")
 public class LayoutController {
 
     @Autowired
